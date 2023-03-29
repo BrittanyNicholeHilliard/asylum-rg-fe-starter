339d3b5c22a8049bc92ece12a4d130606fec88f3
Brittany Hilliard <bnicholehilliard@gmail.com>
2023-03-29 16:44:11 -0700

    api wired

--- src/components/pages/DataVisualizations/GraphWrapper.jsx
+++ src/components/pages/DataVisualizations/GraphWrapper.jsx
@@ -10,11 +10,12 @@ import YearLimitsSelect from './YearLimitsSelect';
 import ViewSelect from './ViewSelect';
 import axios from 'axios';
 import { resetVisualizationQuery } from '../../../state/actionCreators';
-import test_data from '../../../data/test_data.json';
+//import test_data from '../../../data/test_data.json';
 import { colors } from '../../../styles/data_vis_colors';
 import ScrollToTopOnMount from '../../../utils/scrollToTopOnMount';
 
 const { background_color } = colors;
+const URL = 'https://hrf-asylum-be-b.herokuapp.com/cases';
 
 function GraphWrapper(props) {
   const { set_view, dispatch } = props;
@@ -51,61 +52,76 @@ function GraphWrapper(props) {
     }
   }
   function updateStateWithNewData(years, view, office, stateSettingCallback) {
-    /*
-          _                                                                             _
-        |                                                                                 |
-        |   Example request for once the `/summary` endpoint is up and running:           |
-        |                                                                                 |
-        |     `${url}/summary?to=2022&from=2015&office=ZLA`                               |
-        |                                                                                 |
-        |     so in axios we will say:                                                    |
-        |                                                                                 |     
-        |       axios.get(`${url}/summary`, {                                             |
-        |         params: {                                                               |
-        |           from: <year_start>,                                                   |
-        |           to: <year_end>,                                                       |
-        |           office: <office>,       [ <-- this one is optional! when    ]         |
-        |         },                        [ querying by `all offices` there's ]         |
-        |       })                          [ no `office` param in the query    ]         |
-        |                                                                                 |
-          _                                                                             _
-                                   -- Mack 
-    
-    */
+
 
     if (office === 'all' || !office) {
-      axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
+      if (view==='citizenship') {
+      axios.get(`${URL}/citizenshipSummary`, {
           params: {
             from: years[0],
             to: years[1],
           },
-        })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-        })
-        .catch(err => {
+        }).then(res => {
+          stateSettingCallback(view, office, res.data); 
+        }).catch(err => {
           console.error(err);
         });
     } else {
-      axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
+      axios.get(`${URL}/fiscalSummary`, {
+        params: {
+          from: years[0], 
+          to: years[0]
+        }
+      }).then(res => {
+        stateSettingCallback(view, office, res.data);
+      }).catch(err => {
+        console.error(err);
+      });
+      }    
+    }  else {
+      if (view === 'citizenship'){
+        axios.get(`${URL}/citizenshipSummary`, {
           params: {
             from: years[0],
-            to: years[1],
-            office: office,
-          },
-        })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-        })
-        .catch(err => {
+            to: years[1] }
+        }).then(res => {
+          stateSettingCallback(view, office, res.data);
+        }).catch(err => {
+          console.err(err);
+        });
+      } else {
+        axios.get(`${URL}/fiscalSummary`, {
+          params:{
+            from: years[0], 
+            to: years[1] }
+        }).then(res => {
+          stateSettingCallback(view, office, res.data);
+        }).catch(err => {
           console.error(err);
         });
-    }
+      }
+    }  
   }
+
+
+
+  //     axios
+  //       .get(process.env.REACT_APP_API_URI, {
+  //         // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
+  //         params: {
+  //           from: years[0],
+  //           to: years[1],
+  //           office: office,
+  //         },
+  //       })
+  //       .then(result => {
+  //         stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
+  //       })
+  //       .catch(err => {
+  //         console.error(err);
+  //       });
+  //   }
+  // }
   const clearQuery = (view, office) => {
     dispatch(resetVisualizationQuery(view, office));
   };
